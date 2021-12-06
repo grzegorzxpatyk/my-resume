dd4d3e34421a3504c6e23ad68dd0831f6098ee52
Grzegorz Patyk <grzegorz.p4tyk@gmail.com>
2021-12-06 11:39:59 +0100

    change skill level display

--- src/Skill.js
+++ src/Skill.js
@@ -1,10 +1,18 @@
 import React from 'react';
 
 export default function Skill(props) {
+    const blank = 6 - props.level;
+    let dots = '';
+    for (let i = 0; i < props.level; i++) {
+        dots += '+ ';
+    }
+    for (let j = 0; j < blank; j++) {
+        dots += '_ ';
+    }
     return (
         <div className="skill">
             <h4 className="skill-name">{props.name}</h4>
-            <span className="level">{props.level}</span>
+            <span className="level">{dots}</span>
         </div>
     );
 }
