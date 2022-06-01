cd65cad65dfd9bc9cca5f48aec76c0c9e2370769
Grzegorz Patyk <grzegorz.p4tyk@gmail.com>
2022-06-01 14:47:47 +0200

    remove redundant comments

--- src/App.js
+++ src/App.js
@@ -43,14 +43,6 @@ const data = {
             description:
                 'Handling all verbal and written communications with clients and hosting companies. Meeting with clients to review website designs and gather their feedback. Design, build and maintenance of new responsive websites using <em>Wordpress</em>, <em>JavaScript</em>, <em>SASS</em>(with <em>SCSS</em> syntax) and <em>HTML</em>.',
         },
-        // {
-        //     position: 'Junior Architect',
-        //     duration: 'Jul 2020 - Feb 2021',
-        //     company: 'K3 Architektci',
-        //     location: 'Cracow',
-        //     description:
-        //         'Preparation of architectural designs for residential, commercial and public objects using <em>Blender</em> and <em>Adobe Photoshop</em>. Creating visualizations and animations for competition designs. Taking part in meetings with clients.',
-        // },
     ],
     education: [
         {
