9f0cad7c32cac6fba41f117348958a1215919536
Grzegorz Patyk <grzegorzxpatyk@gmail.com>
2023-03-13 11:17:43 +0100

    remove redundant comments in Contact.js

--- src/Contact.js
+++ src/Contact.js
@@ -2,8 +2,6 @@ import React from 'react';
 
 export default function Contact(props) {
     return (
-        // <section className="contact">
-        //     <h2 className="section-title">Contact Info</h2>
         <div className="contact-info">
             <span className="email">
                 <a
@@ -23,18 +21,14 @@ export default function Contact(props) {
                 >
                     <i className="fab fa-linkedin"></i>
                 </a>
-                {/* </span>
-            <span className="social-icons"> */}
                 <a
                     href="https://github.com/grzegorzxpatyk"
                     target="_blank"
                     rel="noopener noreferrer"
                 >
                     <i className="fab fa-github-square"></i>
-                    {/* GitHub */}
                 </a>
             </span>
         </div>
-        // </section>
     );
 }
